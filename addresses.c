a34b762ec472d921f8e972f8a41cb9d1c966a6af
Sean-VM <sicsicrr11@gmail.com>
2026-04-14 00:07:31 +0300

    part1 done

--- addresses.c
+++ addresses.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+//global
 int addr5;
 int addr6;
 
+//functions
 int foo()
 {
     return -1;
 }
 void point_at(void *p);
 void foo1();
+
+//global arg
 char g = 'g';
+
+//function
 void foo2();
 
+//functions
 int secondary(int x)
 {
     int addr2;
@@ -52,12 +58,26 @@ int secondary(int x)
    
     printf("Arrays Mem Layout (T1b):\n");
 
-    /* task 1 b here */
-    
-    printf("Pointers and arrays (T1d): ");
+    /* task 1 b here:*/
+
+    printf("iarray: %p\n",iarray);
+    printf("iarrayplus1: %p\n",iarray+1);
+    printf("farray: %p\n",farray);
+    printf("farrayplus1: %p\n",farray+1);
+    printf("darray: %p\n",darray);
+    printf("darrayplus1: %p\n",darray+1);
+    printf("carray: %p\n",carray);
+    printf("carrayplus1: %p\n",carray+1);
+
+    printf("Pointers and arrays (T1d): \n");
 
     /* task 1 d here */
-    
+    iarray2Ptr = iarray2;
+    carray2Ptr = carray2;
+    printf("iarray: %d,%d,%d\n",*iarray2Ptr,*(iarray2Ptr+1),*(iarray2Ptr+2));
+    printf("carray: %c,%c,%c\n",*carray2Ptr,*(carray2Ptr+1),*(carray2Ptr+2));
+    int* localPtr;
+    printf("uninitialized pointer local variable: %p\n",localPtr);
 
 }
 
@@ -73,7 +93,20 @@ int main(int argc, char **argv)
 	secondary(0);
     
     printf("Command line arg addresses (T1e):\n");
-    /* task 1 e here */
+    /* task 1 e here: */
+    printf("argv: %p\n",argv); //wehre the ptr array of arguments start
+    printf("agrv&: %p\n",&argv); //argv location
+    printf("argv0: %s\n",argv[0]); 
+    printf("agrv0&: %p\n",&argv[0]);
+    printf("argv1: %s\n",argv[1]);
+    printf("agrv1&: %p\n",&argv[1]);
+    printf("argv2: %s\n",argv[2]);
+    printf("agrv2&: %p\n",&argv[2]);
+
+    printf("string adresses:\n");
+    printf("argv0strAdr: %p\n",argv[0]);
+    printf("argv1strAdr: %p\n",argv[1]);
+    printf("argv2strAdr: %p\n",argv[2]);
     
     return 0;
 }
@@ -84,16 +117,17 @@ void point_at(void *p)
     static int addr0 = 2;
     static int addr1;
 
-    long dist1 = (size_t)&addr6 - (size_t)p;
-    long dist2 = (size_t)&local - (size_t)p;
-    long dist3 = (size_t)&foo - (size_t)p;
+    long dist1 = (size_t)&addr6 - (size_t)p; //same section
+    long dist2 = (size_t)&local - (size_t)p; //biggest distance since local var is in the stack(highest) and adr5 is global(lowest)
+    long dist3 = (size_t)&foo - (size_t)p; //code - data distnce medim distance
 
     printf("- dist1: (size_t)&addr6 - (size_t)p: %ld\n", dist1);
     printf("- dist2: (size_t)&local - (size_t)p: %ld\n", dist2);
     printf("- dist3: (size_t)&foo - (size_t)p:  %ld\n", dist3);
     
     printf("Check long type mem size (T1a):\n");
-    /* part of task 1 a here */
+    /* part of task 1 a here: */
+    printf("size is: %d\n",sizeof(dist1));
 
     printf("- addr0: %p\n", &addr0);
     printf("- addr1: %p\n", &addr1);
--- makefile
+++ makefile
@@ -1,3 +1,19 @@
+
+
+
+
+
+
+#==========part1=================
+#link
+addresses : addresses.o
+	gcc -m32 -g -Wall -o addresses addresses.o
+
+#compile count-words.c
+addresses.o : addresses.c
+	gcc -m32 -g -Wall -c -o addresses.o addresses.c
+
+#===========part0=======================
 #link
 count-words : count-words.o
 	gcc -m32 -g -Wall -o count-words count-words.o
@@ -8,4 +24,4 @@ count-words.o : count-words.c
 
 #clean
 clean :
-	rm -f *.o count-words
+	rm -f *.o count-words addresses
