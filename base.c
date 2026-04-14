b85212fe863cd480c6ea394bfa5d4dbc27509cf9
Sean-VM <sicsicrr11@gmail.com>
2026-04-14 17:35:35 +0300

    task 3 start

--- base.c
+++ base.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
  
-
+//task 2a
 char* map(char *array, int array_length, char (*f) (char)){
   char* mapped_array = (char*)(malloc(array_length*sizeof(char)));
   for(int i=0;i<array_length;i++){
@@ -13,8 +13,7 @@ char* map(char *array, int array_length, char (*f) (char)){
  //should the function that calls map free location? 
 
 
-//task2b
-
+//task2b:
 /* Ignores c, reads and returns a character from stdin using fgetc. */
 char my_get(char c){
   return fgetc(stdin);
@@ -31,17 +30,18 @@ char cxprt(char c){
   else{
     printf(". ");
   }
+  //the idea for how to print hex is from stackoverflow:
   printf("%02x\n",(unsigned char)c); //02x used to print in hex
+  //end of inspired code
   return c;
 
 }
 
 
-
 /* Gets a char c and returns its encrypted form by adding 1 to its value. If c is 0x7F this wraps around and 0x20 is returned.
 If c is not between 0x20 and 0x7F it is returned unchanged */
 char encrypt(char c){
-  if(c>=0x20 && c<=0x7F){
+  if(c>=0x20 && c<0x7F){
     return(c+1);
   }
   if(c==0x7F){
@@ -52,12 +52,10 @@ char encrypt(char c){
 
 
 
-
-
 /* Gets a char c and returns its decrypted form by reducing 1 from its value. 
 If c is 0x20 this wraps around and 0x7F is returned. If c is not between 0x20 and 0x7F it is returned unchanged: */
 char decrypt(char c){
-   if(c>=0x20 && c<=0x7F){
+   if(c>0x20 && c<=0x7F){
     return(c-1);
   }
   if(c==0x20){
@@ -68,10 +66,7 @@ char decrypt(char c){
 
 
 
-
-
-
-/* dprt prints the value of c in a decimal representation followed by a new line, and returns c unchanged. */
+/* dprt prints the value of c in a decimal representation followed by a new line, and returns c unchanged: */
 char dprt(char c){
   printf("%d \n",c);
   return c;
@@ -88,40 +83,46 @@ char index;
 char (*fun)(char);
 };
 
-struct fun_desc menu1[] = { { "<m>y_get", 'm', my_get}, { "<c>xprt", 'c',cxprt }, { "<e>ncrypt", 'e',encrypt }, { "<d>ecrypt", 'd',decrypt },  { "<d>prt", 'd',dprt },{ NULL, 0, NULL } };
-int arrayLen = 6;
+struct fun_desc menu1[] = { { "<m>y_get", 'm', my_get}, { "<c>xprt", 'c',cxprt }, { "<e>ncrypt", 'e',encrypt }, { "<d>ecrypt", 'd',decrypt },  { "<p>dprt", 'p',dprt },{ NULL, 0, NULL } };
 void menu(){
-
-     while(feof(stdin)==0){//we have more 
-        fprintf(stdout,"Select operation from the following menu by index:");
-        for(int i=0;i<arrayLen;i++){
-            printf(menu1[i].name);
-          }
-        char* carray;
-        //code taken from tuturials point
-        char buffer[50];
-        fgets(buffer, sizeof(buffer), stdin);
-        //end of taken code
-        for(int i=0;i<arrayLen;i++){
-          if(strcmp(buffer,menu1[i].index) == 0){
-            char* tmp =  map(buffer,50,menu1[i].fun);
-            carray= tmp;
-            free(tmp);
-            free(carray);
-          }
+  char* carray = (char*)(malloc(5));
+  //init to empty string:
+  char[0]='\0' //null charachter 
+    while(feof(stdin)==0){//we have more 
+      fprintf(stdout,"Select operation from the following menu by index:");
+      int idx=0;
+      while(menu1[index].fun!=NULL){
+          printf(menu1[idx].name);
+          printf("\n");
+          idx++;
+      }
+      
+      //code taken from tutorialsPoint:
+      char buffer[50]; //array of chars
+      fgets(buffer, sizeof(buffer), stdin);
+      //end of taken code
+      int isError = 1;
+      for(int i=0;i<arrayLen;i++){
+        if(strcmp(buffer[0],menu1[i].index) == 0){
+          char* tmp =  map(carray,5,menu1[i].fun);
+          free(carray);
+          carray= tmp;
+          isError = 0;
         }
-        printf("function not supported");
-    }   
-    exit(0);
+        if(isError==1){
+           printf("function not supported");
+        }
+       
+      }
+      
+  }   
+  exit(0);
 }
 
+
+
+
 /*
-
-
-
-
-
-
 A user should be able to terminate the program when asked for a choice, by signaling EOF using 'Ctrl+D' on an empty line.
 
 For example, the menu structure initialized as the menu[]
